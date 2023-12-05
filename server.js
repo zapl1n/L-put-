745ed0d02a1868e450f3b4a73147b4b4088b6907
zapl1n <zapl1n@github.com>
2023-12-05 10:14:56 +0200

    Struktuur

---
+++ server.js
@@ -0,0 +1,27 @@
+const express = require('express');
+
+const app = express();
+
+app.use(express.static('public'));
+
+
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + '/views/index.html');
+}),
+app.get('/about', (req, res) => {
+    res.sendFile(__dirname + '/views/lost.html');
+}),
+app.get('/works', (req, res) => {
+    res.sendFile(__dirname + '/views/found.html');
+}),
+app.delete('/', (req, res) => {
+    res.send();
+})
+
+
+
+
+app.listen(3000, () => {
+    console.log('Server running on port 3000');
+}
+);
