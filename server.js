38794efbdc4d8a6c56576cb4b011a32c6cfa6d88
zapl1n <zapl1n@github.com>
2023-12-05 13:19:00 +0200

    Struktuur

--- server.js
+++ server.js
@@ -8,15 +8,15 @@ app.use(express.static('public'));
 app.get('/', (req, res) => {
     res.sendFile(__dirname + '/views/index.html');
 }),
-app.get('/about', (req, res) => {
-    res.sendFile(__dirname + '/views/lost.html');
-}),
-app.get('/works', (req, res) => {
-    res.sendFile(__dirname + '/views/found.html');
-}),
-app.delete('/', (req, res) => {
-    res.send();
-})
+    app.get('/about', (req, res) => {
+        res.sendFile(__dirname + '/views/lost.html');
+    }),
+    app.get('/works', (req, res) => {
+        res.sendFile(__dirname + '/views/found.html');
+    }),
+    app.delete('/', (req, res) => {
+        res.send();
+    })
 
 
 
